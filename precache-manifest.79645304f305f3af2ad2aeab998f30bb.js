71e0499b1ab305cd53e720ba2e255d9b7d65ebaf
marko1996dj <marko96djuric@gmail.com>
2019-04-10 14:08:11 +0200

    Updates

---
+++ precache-manifest.79645304f305f3af2ad2aeab998f30bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "29abe0de55f5c9f61913e34eec0214d3",
+    "url": "/nail-salon/static/media/wp.29abe0de.png"
+  },
+  {
+    "revision": "1ae85442648b70645ab56078f7b09a02",
+    "url": "/nail-salon/static/media/map.1ae85442.png"
+  },
+  {
+    "revision": "f782b3732d3655eb6c99",
+    "url": "/nail-salon/static/js/runtime~main.375f3d1e.js"
+  },
+  {
+    "revision": "11e9314f5e5428206763",
+    "url": "/nail-salon/static/js/main.4118c7c3.chunk.js"
+  },
+  {
+    "revision": "c1d58ef64831a7a22350",
+    "url": "/nail-salon/static/js/2.4b4bf3f9.chunk.js"
+  },
+  {
+    "revision": "11e9314f5e5428206763",
+    "url": "/nail-salon/static/css/main.14d6f934.chunk.css"
+  },
+  {
+    "revision": "3a1722c6dfe260b12a0e5f1ebeb9437f",
+    "url": "/nail-salon/index.html"
+  }
+];
